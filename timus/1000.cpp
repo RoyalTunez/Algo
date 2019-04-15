e1f992ba66380de6ab3b03c01281cb34ac6737aa
RoyalTunez <40517196+RoyalTunez@users.noreply.github.com>
2019-04-15 15:28:44 +0300

    Create 1000.cpp

---
+++ timus/1000.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <stdio.h>
+
+using namespace std;
+unsigned long long a, b;
+
+int main(void) {
+    cin >> a >> b;
+    cout << a + b;
+    return 0;
+}
